b5fb31273ff4ab97e5c464724b94e9e560ab149a
juan jose <juanjosenarvaezmarin13092005@gmail.com>
2026-07-03 01:35:42 -0500

    Initial project setup and package structure

---
+++ src/main/java/com/juanjose/ganadex/GanadexApplication.java
@@ -0,0 +1,13 @@
+package com.juanjose.ganadex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanadexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanadexApplication.class, args);
+	}
+
+}
